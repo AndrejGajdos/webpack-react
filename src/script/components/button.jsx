cbfe7b148fde9d37c6dac390e3fb1f91e7b48107
Alex Jacobs <lex.jacobs@gmail.com>
2016-08-15 17:18:09 -0700

    Simplifies route to bootstrap.css
    Require without relative path indicators defaults to node_modules folder.

--- src/script/components/button.jsx
+++ src/script/components/button.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var buttonStyle = require('./button.scss');
 
 var bootstrap = require('bootstrap');
-var bootstrapStyle = require("../../../node_modules/bootstrap/dist/css/bootstrap.css");
+var bootstrapStyle = require("bootstrap/dist/css/bootstrap.css");
 
 export class Button extends React.Component {
   render() {
