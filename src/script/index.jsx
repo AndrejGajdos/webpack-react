ff6bd6e2bb6559bc270edb0b2f7446275b561c8e
mert inan <inanme@gmail.com>
2016-12-15 05:47:03 +0000

    ES6 style imports

--- src/script/index.jsx
+++ src/script/index.jsx
@@ -1,6 +1,6 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Button = require('./components/button').Button;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from './components/button';
 
 ReactDOM.render(
-  <Button/>, document.getElementById('content'));
+    <Button/>, document.getElementById('content'));
